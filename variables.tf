38de041b0922c4b37c1f31cabd9d704ed4074b8f
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-04-08 20:29:56 -0400

    Added new variable

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "private_key" {
   sensitive   = true
   description = "The private key to access home lab machines."
 }
+
+variable "tenzin_io_github_app_key" {
+  type        = string
+  sensitive   = true
+  description = "The private key for tenzin.io GitHub application"
+}
